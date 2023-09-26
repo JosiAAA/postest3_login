babfb78c9c6f801a776fa5ff1001367cbf294445
josiAAA <josiapardosi@gmail.com>
2023-09-26 21:23:26 +0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.base')
+
+@section('content')
+       <div class="welcome-page">
+    
+          
+            
+            <h1>Selamat Datang</h1>
+            <p>Di Bandara JoAirlines</p>
+          
+       </div>
+@endsection    
+
